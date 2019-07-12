a4f29eec2690496a75a24d55ad306a6835a6712d
btjony28 <52534824+btjony28@users.noreply.github.com>
2019-07-12 12:20:21 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,16 @@
 function mostrar()
 {
-	//Genero el número RANDOM entre 1 y 10 
-	
+  //Genero el número RANDOM entre 1 y 10
+  var numero;
+  numero = Math.floor(Math.random()*(11-1)+1)
+  if(numero>8){alert(numero+" excelente")
+}else{
+  if(numero>4){
+    alert(numero+" aprobo");
+  }else{
+    alert(numero+" vamos la proxima se puede");
+  }
+}
+
 
 }//FIN DE LA FUNCIÓN
