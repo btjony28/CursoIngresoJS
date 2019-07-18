458b8332b755fc09ca399dd72319887e2c0a3a96
btjony28 <52534824+btjony28@users.noreply.github.com>
2019-07-18 00:39:34 -0300

    trabajo en clase

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -6,7 +6,16 @@ var mesDelAño = document.getElementById('mes').value;
 switch(mesDelAño){
   case "Febrero":
    alert("Febrero tiene 28 dias");
-  break;
+   break;
+  case "Abril":
+  case "Junio":
+  case "Septiembre":
+  case "Noviembre":
+    alert(mesDelAño+" tiene 30 dias");
+    break;
+  default:
+    alert(mesDelAño+" tiene 31 dias");
+    break;
 
 }
 //alert (mesDelAño);
--- 3-InstruccionSwitch/jsInstruccionSwitch(5).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(5).js
@@ -1,7 +1,22 @@
 function mostrar()
 {
 //tomo la edad  
-var laHora = document.getElementById('hora').value;
+var laHora;
+laHora=document.getElementById("hora").value;
+
+switch(laHora){
+  case "7":
+  case "8":
+  case "9":
+  case "10":
+  case "11":
+  alert("es de mañana");
+  break;
+  default:
+  break;
+}
+
+
 
 //alert (laHora);
 	
