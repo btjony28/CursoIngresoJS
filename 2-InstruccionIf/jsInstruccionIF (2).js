1f33c958a78bf6542d04476447e7be92bbe66ea6
btjony28 <52534824+btjony28@users.noreply.github.com>
2019-07-18 00:38:06 -0300

    trabajo en clase

--- 2-InstruccionIf/jsInstruccionIF (2).js
+++ 2-InstruccionIf/jsInstruccionIF (2).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
+var edad;
+
+edad= document.getElementById("edad").value;
+if(edad>17){alert(edad);}
+
 
 
 
--- 2-InstruccionIf/jsInstruccionIF (3).js
+++ 2-InstruccionIf/jsInstruccionIF (3).js
@@ -1,6 +1,11 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
+var edad;
+
+edad = document.getElementById("edad").value;
+if(edad>17){alert("persona mayor de edad");}
+else{alert("persona es menor de edad");}
 
 
 }//FIN DE LA FUNCIÓN
